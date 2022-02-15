b6f12b4d5c64547cd8b5bd6dc98b7fb05e1e0576
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-02-15 08:51:07 -0600

    js update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName 
+
+console.log("This is a string, good for leaving a message");
+//this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+//this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
