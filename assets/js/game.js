1e00d918404121f4c964f1d1c991f8ad86c6e41a
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-02-15 23:17:31 -0600

    roboglad

--- assets/js/game.js
+++ assets/js/game.js
@@ -64,3 +64,24 @@ var fight = function() {
   }
 };
 fight();
+
+// function to start a new game
+var startGame = function() {
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+};
+//start the game when the page loads
+startGame();
