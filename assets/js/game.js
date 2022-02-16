1e05e8bce6dfb179e1b49766626dbcb18ed83418
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-02-16 17:28:57 -0600

    changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,8 +80,28 @@ var startGame = function() {
     else {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
+    }//after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    // function to end the entire game
+    var endGame = function() {
+      // if player is still alive, player wins!
+      if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+      } 
+      else {
+        window.alert("You've lost your robot in battle.");
+      }
+      // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+     
+    if (playAgainConfirm) {
+    // restart the game
+      startGame();
+    } 
+    else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
   }
-};
+  }
+}
 //start the game when the page loads
 startGame();
